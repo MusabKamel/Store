6dd80e1947ff409ad455d69bf01d9df5b19ff3b1
musab <63923768+MusabKamel@users.noreply.github.com>
2024-01-17 01:22:44 +0200

    Add files via upload
    main page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Sing_in/sign_in.html", "./Main/main.html"],
+  theme: {
+    fontFamily: {
+      MyFont: ["font-family: 'Cairo', sans-serif;"],
+    },
+    //    colors: {
+    //     main_color: "#266073",
+    //  },
+    extend: {},
+  },
+  plugins: [],
+};
